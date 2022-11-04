0a88392842459aa89f2e559ab3c5cefbba55fa2a
hab506 <hab506@usask.ca>
2022-11-04 16:47:09 -0600

    code added in enemy script
    enemy health and damage to player code added

--- Assets/Scripts/Skeletons.cs
+++ Assets/Scripts/Skeletons.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Skeletons : MonoBehaviour
 {
     public GameObject player;
-        [SerializeField] private float moveSpeed = 1.0f; 
+    [SerializeField] private float moveSpeed = 1.0f; 
+    [SerializeField] private float health = 100.0f; 
 
+    [SerializeField] private float damageToPlayer = 20.0f;
+    [SerializeField] private float damageRate = 0.2f;
+    [SerializeField] private float damageTime; 
 
     // Start is called before the first frame update
     void Start()
@@ -15,21 +19,36 @@ public class Skeletons : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    
+
+     void Update()
 
     {
         Movement();
       
     }
 
-
-    private void Movement () {
+     private void Movement () {
 
         Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, moveSpeed * Time.deltaTime);
         transform.position += transform.forward * 1f * Time.deltaTime;
-
-       
         
     }
+
+     public void TakeDamage (float damage) {
+        health -= damage; 
+
+        if (health <= 0) {
+            Destroy(this.gameObject); 
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (other.transform.tag == "Player" && Time.time > damageTime) {
+            other.transform.GetComponent<Player>().health = other.transform.GetComponent<Player>().health - 20f; 
+            damageTime = Time.time + damageRate;             
+        }        
+    }
 }
+
--- Assets/Scripts/Zombies.cs
+++ Assets/Scripts/Zombies.cs
@@ -8,6 +8,11 @@ public class Zombies : MonoBehaviour
     public GameObject player;
     // private NavMeshAgent enemy;
     [SerializeField] private float moveSpeed = 1.0f; 
+    [SerializeField] private float health = 100.0f; 
+
+    [SerializeField] private float damageToPlayer = 20.0f;
+    [SerializeField] private float damageRate = 0.2f;
+    [SerializeField] private float damageTime; 
     
     void Start()
     {
@@ -27,14 +32,21 @@ public class Zombies : MonoBehaviour
         Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, moveSpeed * Time.deltaTime);
         transform.position += transform.forward * 1f * Time.deltaTime;
-
-       
-
-        // if (player) { //null reference check
-        //     transform.LookAt(player.transform.position); //Look at the player
-        //     transform.position += transform.forward * moveSpeed * Time.deltaTime;
-             
-        // }
         
+    }
+
+     public void TakeDamage (float damage) {
+        health -= damage; 
+
+        if (health <= 0) {
+            Destroy(this.gameObject); 
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (other.transform.tag == "Player" && Time.time > damageTime) {
+            other.transform.GetComponent<Player>().health = other.transform.GetComponent<Player>().health - 20f; 
+            damageTime = Time.time + damageRate;             
+        }        
     }
 }
