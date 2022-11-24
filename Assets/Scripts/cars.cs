d04b41c898b50df84d00ef0dba0a52d329ebb52b
sony <son941@usask.ca>
2022-11-23 22:49:01 -0600

    cars

--- Assets/Scripts/cars.cs
+++ Assets/Scripts/cars.cs
@@ -7,9 +7,9 @@ public class cars : MonoBehaviour
     public GameObject player;
 
     [SerializeField] private float Speed = 8.0f;
-    [SerializeField] private float health = 100.0f;
+    //[SerializeField] private float health = 100.0f;
 
-    [SerializeField] private float damageToPlayer = 20.0f;
+    [SerializeField] private float damageToPlayer = -9999.0f;
     [SerializeField] private float damageRate = 0.2f;
     [SerializeField] private float damageTime;
 
@@ -33,22 +33,12 @@ public class cars : MonoBehaviour
 
     }
 
-    public void TakeDamage(float damage)
+    void OnTriggerStay(Collider other)
     {
-        health -= damage;
-
-        if (health <= 0)
+        if (other.transform.tag == "Player" && Time.time > damageTime)
         {
-            Destroy(this.gameObject);
+            other.transform.GetComponent<Player>().TakeDamage(damageToPlayer);
+            damageTime = Time.time + damageRate;
         }
     }
-
-    //void OnTriggerStay(Collider other)
-    //{
-    //    if (other.transform.tag == "Player" && Time.time > damageTime)
-    //    {
-    //        other.transform.GetComponent<Player>().TakeDamage(damageToPlayer);
-    //        damageTime = Time.time + damageRate;
-    //    }
-    //}
 }
