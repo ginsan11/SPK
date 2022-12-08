fcd3da69fa1dc6e200b825dbab96c9d4ef9b7895
hab506 <hab506@usask.ca>
2022-12-07 22:23:31 -0600

    spawner bugs fixed

--- Assets/Scripts/SkeletonSpawner.cs
+++ Assets/Scripts/SkeletonSpawner.cs
@@ -10,15 +10,20 @@ public class SkeletonSpawner : MonoBehaviour
     [SerializeField] private float spawnRate = 5.5f;
 
     private float spawnTimer;
+    public GameObject player;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         EnnemySpawner();
     }
  
@@ -26,12 +31,32 @@ public class SkeletonSpawner : MonoBehaviour
     {
         if (Time.time > spawnTimer)
         {
-            // int randSpawnPoints = Random.Range(0, spawnPoints.Length);  
+        
 
-            transform.position = GameManager.instance.player.transform.position + new Vector3(3,0,3);
+            transform.position = player.transform.position + new Vector3(3,0,3);
 
             Instantiate(SkeletonPrefab, transform.position, transform.rotation);
+            // PlayerPos();
             spawnTimer = Time.time + spawnRate;
         }
     }
+
+
+    // void PlayerPos(){
+
+    //     Vector3 playerPos = GameManager.instance.player.transform.position;
+    //     Vector3 playerDirection = GameManager.instance.player.transform.forward;
+    //     Quaternion playerRotation = GameManager.instance.player.transform.rotation;
+    //     float spawnDistance = 3;
+    //     Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
+    //     Instantiate(SkeletonPrefab, spawnPos, transform.rotation);
+
+    // }
+
+    // Vector3 PlayerPos(){
+    //     Vector3 playerpos =  GameManager.instance.player.transform.position + new Vector3(3,0,3);
+    //     return playerpos;
+    // }
+
+
 }
